dec505fc3136a3d86bbd344145f66abe9d20ec92
ZYX31 <jasiusyl@gmail.com>
2022-11-18 20:21:58 +0100

    Done

--- app/src/main/java/com/example/zadanienaocene/MainActivity.kt
+++ app/src/main/java/com/example/zadanienaocene/MainActivity.kt
@@ -7,7 +7,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
-
+//Jan Syldatk
 class MainActivity : AppCompatActivity() {
     private var swipe = true
     @SuppressLint("MissingInflatedId")
